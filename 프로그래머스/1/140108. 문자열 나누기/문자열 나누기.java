cf01409231b6e0d68751b19289a48d74d8bccf96
MoonSung Bae <qoanstjdsla@gmail.com>
2026-09-27 02:22:15 +0900

    [level 1] Title: 문자열 나누기, Time: 0.63 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        ArrayList<String> answer = new ArrayList<>();
+        int answer = 0;
         
         int iam = 0;
         int you = 0;
-        int standardIdx = 0;
         char standardChar = s.charAt(0);
         
         for(int i = 0; i<s.length(); i++) {
             
             if(iam == 0 && you == 0) {
-                standardIdx = i;
                 standardChar = s.charAt(i);
             }
             
@@ -23,23 +21,19 @@ class Solution {
             }
             
             if(iam == you) {
-                answer.add(s.substring(standardIdx, i+1));
+                answer++;
                 iam = 0;
                 you = 0;
             }
             
             else if(i == s.length() -1) {
-                answer.add(s.substring(standardIdx, i+1));
+                answer++;
             }
             
          
         }
         
-        for(String fuck : answer) {
-            System.out.println(fuck);
-        }
         
-        
-        return answer.size();
+        return answer;
     }
 }
