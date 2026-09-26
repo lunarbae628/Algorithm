9cd9f05dae11c8517fc9e997b51ae3a0ad345475
MoonSung Bae <qoanstjdsla@gmail.com>
2026-09-27 00:23:31 +0900

    [level 1] Title: 신고 결과 받기, Time: 103.37 ms, Memory: 177 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,29 +4,28 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         
         int[] answer = new int[id_list.length];
+        HashMap<String, Integer> answerIdxMap = new HashMap<>();
+        HashMap<String, HashSet<String>> reported = new HashMap<>();
         
-        Map<String, Integer> reported = new HashMap<>();
-        Map<String, Integer> answerIdx = new HashMap<>();
-        Set<String> uniqueReports = new HashSet<>(Arrays.asList(report));
-        
-        for (int i = 0; i < id_list.length; i++) {
-            reported.put(id_list[i], 0);
-            answerIdx.put(id_list[i], i);
+        for(int i = 0; i < id_list.length; i++) {
+            answerIdxMap.put(id_list[i],i);
+            reported.put(id_list[i], new HashSet<>());
         }
         
-        for (String r : uniqueReports) {
-            String[] tmp = r.split(" ");
-            reported.put(tmp[1], reported.get(tmp[1]) + 1);
-        } 
-        
-        for (String r : uniqueReports) {
-            String[] parts = r.split(" ");
-            String reporter = parts[0];
-            String reportedUser = parts[1];
+        for(String r : report) {
+            String[] pair = r.split(" ");
             
-            if (reported.get(reportedUser) >= k) {
-                int reporterIdx = answerIdx.get(reporter);
-                answer[reporterIdx]++;
+            HashSet<String> set = reported.get(pair[1]);
+            
+            set.add(pair[0]);
+        }
+        
+        
+        for(HashMap.Entry<String, HashSet<String>> entry : reported.entrySet()) {
+            if(entry.getValue().size() >= k) {
+                for(String s : entry.getValue()) {
+                    answer[answerIdxMap.get(s)]++;
+                }
             }
         }
         
