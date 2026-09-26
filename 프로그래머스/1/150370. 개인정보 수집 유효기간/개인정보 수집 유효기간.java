e0b9aa3bb3339486924069693b4c3a8ec9e07150
MoonSung Bae <qoanstjdsla@gmail.com>
2026-09-26 23:23:29 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.17 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,43 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
         
-        String[] todays = today.split("\\.");
+        ArrayList<Integer> answer = new ArrayList<>();
         
-        int todayInt = toDays(todays);
+        HashMap<String, Integer> map = new HashMap<>();
         
-        Map<String, Integer> termMap = new HashMap<>();
+        String[] todayList = today.split("\\.");
         
-        for(int i = 0; i < terms.length; i++) {
-            String termKind = terms[i].substring(0,1);
-            Integer termExpire = Integer.parseInt(terms[i].substring(2)) * 28;
-            System.out.println(termKind + termExpire);
-            termMap.put(termKind, termExpire);
+        int todayNum = getDates(todayList);
+        
+        for(String t : terms) {
+            String[] term = t.split(" ");
+            map.put(term[0], Integer.parseInt(term[1]) * 28);
         }
         
-        for(int j = 0; j < privacies.length; j++) {
-            String[] privaciesDate = privacies[j].substring(0,10).split("\\.");
-            int privaciesDateInt = toDays(privaciesDate);
+        for(int i = 0; i<privacies.length; i++) {
+            String[] pri = privacies[i].split(" ");
             
-            String privaciesKind = privacies[j].substring(11);
-            
-            if(termMap.get(privaciesKind) + privaciesDateInt <= todayInt) {
-                answer.add(j+1);
+            if(todayNum - getDates(pri[0].split("\\.")) >= map.get(pri[1])) {
+                answer.add(i+1);
             }
-
-        } 
-
-        return answer.stream().
-            mapToInt(Integer::intValue)
-            .toArray();
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+            
     }
     
-    
-    
-    
-    
-    public int toDays(String[] strDay) {
-        return (Integer.parseInt(strDay[0]) * 12 * 28) + (Integer.parseInt(strDay[1]) * 28) + Integer.parseInt(strDay[2]);
+    private int getDates(String[] date) {
+        return (Integer.parseInt(date[0]) * 12 * 28) + (Integer.parseInt(date[1]) * 28) + Integer.parseInt(date[2]);
     }
 }
